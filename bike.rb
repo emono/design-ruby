2af6348d3b32c4068978baf314c8da3f363eee5b
emono <ababababa0222@gmail.com>
2019-05-06 02:14:32 +0900

    change directroy

--- bike.rb
+++
@@ -1,3 +0,0 @@
-require './gear'
-require './wheel'
-puts Gear.new(52, 11, 26, 1.5).gear_inches
---
+++ design_ruby.rb
@@ -0,0 +1,3 @@
+require './lib/gear'
+require './lib/wheel'
+puts Gear.new(52, 11, 26, 1.5).gear_inches
