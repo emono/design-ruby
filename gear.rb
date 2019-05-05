f930f59bec7cb508857e9408f257e814c0f9702f
emono <ababababa0222@gmail.com>
2019-05-05 19:32:23 +0900

    refactoring gear

--- gear.rb
+++ gear.rb
@@ -1,21 +1,26 @@
 class Gear
-  attr_accessor :chainring, :cog, :rim, :tire
+  attr_reader :chainring, :cog, :wheel
   def initialize(chainring, cog, rim, tire)
     @chainring = chainring
     @cog = cog
-    @rim = rim
-    @tire = tire
+    @wheel = Wheel.new(rim, tire)
   end
   
   def ratio
     chainring / cog.to_f
   end
 
-  def gea_inches
+  def gear_inches
     # タイヤはリムの周りを囲むので、直径を計算するためには2倍する
-    ratio * (rim + (tire * 2))
+    ratio * wheel.diameter
+  end
+
+  Wheel = Struct.new(:rim, :tire) do
+    def diameter
+      rim + (tire * 2)
+    end
   end
 end
 
-puts Gear.new(52, 11, 26, 1.5).gea_inches
-puts Gear.new(30, 27, 24, 1.25).gea_inches
+puts Gear.new(52, 11, 26, 1.5).gear_inches
+puts Gear.new(30, 27, 24, 1.25).gear_inches
