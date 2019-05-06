ff96638f8e2952aaad6572cd1b8e3f7eb3c62449
emono <ababababa0222@gmail.com>
2019-05-06 10:44:37 +0900

    dafult args use hash merge

--- lib/gear.rb
+++ lib/gear.rb
@@ -1,10 +1,18 @@
 class Gear
   attr_reader :chainring, :cog, :rim,  :tire
   def initialize(args)
-    @chainring = args.fetch(:chainring, 40)
-    @cog = args.fetch(:cog, 40)
+    args = default.merge(args)
+    @chainring = args[:chainring]
+    @cog = args[:cog]
     @rim = args[:rim]
     @tire = args[:tire]
+  end
+
+  def default 
+    {
+      chainring: 40,
+      cog: 18
+    }
   end
   
   def gear_inches
