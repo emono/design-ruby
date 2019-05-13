9217f0e18de96a1137be2dfb747b2953dc32339e
emono <ababababa0222@gmail.com>
2019-05-13 11:02:10 +0900

    add driver, trip_coordinator

--- lib/trip.rb
+++ lib/trip.rb
@@ -1,7 +1,18 @@
 Class Trip
   attr_reader :bicycles, :customers, :vehicle
   # この'mecanic'の引数はどんなクラスのものでもいい
-  def prepare
+  def prepare(prepares)
+    prepares.each{|prepare|
+      case prepare
+      when Mecanic
+        prepara.prepare_bicycles(bicycles)
+      when TripCoordinator
+        prepara.buy_food(customers)
+      when Driver
+        prepare.gas_up(vehicle)
+        prepare.fill_water_tand(vehicle)
+      end
+    }
     mecanic.prepare_bicycles(bicycle)
   end
 end
